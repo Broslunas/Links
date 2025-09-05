b4639d666bd77d01b751b61c68e548927e3378f7
Broslunas <pablo@broslunas.com>
2025-09-05 10:51:39 +0100

    changes

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,10 @@ export { default as User } from './User';
 export { default as Link } from './Link';
 export { default as AnalyticsEvent } from './AnalyticsEvent';
 export { default as TempExport } from './TempExport';
-// MaintenanceState model has been removed
 export { default as Dashboard } from './Dashboard';
 
 export type { IUser } from './User';
 export type { ILink } from './Link';
 export type { IAnalyticsEvent } from './AnalyticsEvent';
 export type { ITempExport } from './TempExport';
-// IMaintenanceState type has been removed
 export type { IDashboard } from './Dashboard';
